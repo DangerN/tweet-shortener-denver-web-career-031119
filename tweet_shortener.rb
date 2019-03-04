a5dfc6eb6b3b0eeb2ed8d0b27ded478197630193
convivial-system-4430 <cdneal091@gmail.com>
2019-03-04 00:26:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  dictionary = {
+    'hello' => 'hi',
+    'to' => '2',
+    'two' => '2',
+    'too' => '2',
+    'begin' => 'b',
+    'you' => 'u',
+    'at' => '@',
+    'and' => '&',
+  }
+end
+
+def word_substituter(tweet_one)
+  new_tweet = tweet_one.split(' ')
+  binding.pry
+  new_tweet.each.with_index do |word, i|
+    if dictionary.keys.include?(word)
+      new_tweet[i] = dictionary[word]
+    end
+  end
+      
+    
+  new_tweet.join(' ')
+end
+
+def bulk_tweet_shortener(tweets)
+end
+
+def selective_tweet_shortener(tweet_one)
+end
+
+def shortened_tweet_truncator(tweet_three)
+end
+
